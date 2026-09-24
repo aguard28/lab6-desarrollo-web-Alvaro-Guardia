73eb15295a65310a338b2dcb704ae5b9c8f48459
aguard28 <x.aguard28@gmail.com>
2026-09-24 18:05:03 -0500

    Add files via upload

---
+++ lab6/js/app.js
@@ -0,0 +1,10 @@
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual';
+}
+
+window.addEventListener('load', function() {
+  window.scrollTo(0, 0);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+});
